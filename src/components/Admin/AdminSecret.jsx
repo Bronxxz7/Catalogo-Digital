12ad40aa6927d5783a5689afda7f23f278bf3bc4
Bronxxz7 <b72517201@gmail.com>
2026-05-11 21:53:46 -0500

    cambio de número

--- src/components/Admin/AdminSecret.jsx
+++ src/components/Admin/AdminSecret.jsx
@@ -1,3 +1,4 @@
+ HEAD
 import { LockKeyhole, ShieldCheck } from "lucide-react";
 
 export default function AdminSecret({
@@ -17,4 +18,25 @@ export default function AdminSecret({
       <LockKeyhole size={16} />
     </button>
   );
+
+import { LockKeyhole, ShieldCheck } from "lucide-react";
+
+export default function AdminSecret({
+  setPage,
+}) {
+  return (
+    <button
+      className="admin-secret-btn"
+      onClick={() => setPage("admin")}
+    >
+      <div className="secret-glow"></div>
+
+      <ShieldCheck size={18} />
+
+      <span>Panel privado</span>
+
+      <LockKeyhole size={16} />
+    </button>
+  );
+}
 }
