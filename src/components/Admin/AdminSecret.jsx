7a89b5c680d94554557154b3378c2cf60ab7fd36
Bronxxz7 <b72517201@gmail.com>
2026-05-11 21:45:26 -0500

    conflicto

--- src/components/Admin/AdminSecret.jsx
+++ src/components/Admin/AdminSecret.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { LockKeyhole, ShieldCheck } from "lucide-react";
 
 export default function AdminSecret({
@@ -18,25 +17,4 @@ export default function AdminSecret({
       <LockKeyhole size={16} />
     </button>
   );
-=======
-import { LockKeyhole, ShieldCheck } from "lucide-react";
-
-export default function AdminSecret({
-  setPage,
-}) {
-  return (
-    <button
-      className="admin-secret-btn"
-      onClick={() => setPage("admin")}
-    >
-      <div className="secret-glow"></div>
-
-      <ShieldCheck size={18} />
-
-      <span>Panel privado</span>
-
-      <LockKeyhole size={16} />
-    </button>
-  );
->>>>>>> d0835dcd45448d3197509d5f9c0e54e3e4e60d43
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp } from "firebase/app";
 
 import {
@@ -22,29 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-=======
-import { initializeApp } from "firebase/app";
-
-import {
-  getFirestore
-} from "firebase/firestore";
-
-import {
-  getAuth
-} from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAdcNZsInSwgk-z-Ica7R_b89KgXef7mqc",
-  authDomain: "streaming-pe.firebaseapp.com",
-  projectId: "streaming-pe",
-  storageBucket: "streaming-pe.firebasestorage.app",
-  messagingSenderId: "106021969684",
-  appId: "1:106021969684:web:e63ff2b4e2d1adb732ec01",
-  measurementId: "G-9GSX96VMFR"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
->>>>>>> d0835dcd45448d3197509d5f9c0e54e3e4e60d43
 export const auth = getAuth(app);
